4b2905208cc8b199a8aad6b5d058e51841f14421
A-nuchit <MCB@Anuchits-MacBook-Pro.local>
2017-12-23 15:19:36 +0700

    first commit

---
+++ time_chack.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int Hs,Ms,He,Me,Hn,Mn;
+	printf("Enter time start : ");
+	scanf("%d:%d",Hs,Ms);
+	printf("Enter time end : ");
+	scanf("%d:%d",He,Me);
+	
+	return 0;
+}
